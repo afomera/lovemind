4f40f4c0803abf60c6d4ae509e0783f4473a3efb
James Jelinek <jelinek@gmail.com>
2015-08-08 12:44:10 -0500

    Leverage sucker_punch for active_job delay blah blah

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -18,7 +18,7 @@ class ResourcesController < ApplicationController
 
     respond_to do |format|
       if @resource.save
-				ResourceMailer.submission(@resource).deliver
+				ResourceMailer.submission(@resource).deliver_later
         format.html { redirect_to @resource, notice: "Hey #{current_user.email}, #{@resource.name} was successfully added." }
         format.json { render :show, status: :created, location: @resource }
       else
