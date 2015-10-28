8aac5f49e91544358e53cc0778391e97a7f20a70
James Jelinek <jelinek@gmail.com>
2015-10-27 19:13:28 -0500

    Updated stuff

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,8 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @resource, notice: "Hey #{current_user.username}, your comment was successfully added." }
-        format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
