4380e8c5824f1437e2b5072c8127ec054e4655d8
ShakyCode <jelinek@gmail.com>
2015-12-19 15:26:20 -0600

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -24,14 +24,6 @@ ActiveRecord::Schema.define(version: 20151027223900) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "facilities", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
-    t.text     "notes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
     t.integer  "sluggable_id",              null: false
